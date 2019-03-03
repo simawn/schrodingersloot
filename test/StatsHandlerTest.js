35a9e4462a2b75dbce0f3ba02fe85f67c500bc6d
Simon Huang <simon@simonhuang.ca>
2019-03-02 20:19:49 -0500

    Started test cases for StatsHandler.js

---
+++ test/StatsHandlerTest.js
@@ -0,0 +1,23 @@
+describe("Testing local storage of DataHandler", () => {
+    it("should create the correct entry in localStorage", () => {
+        //TODO
+        fail("Test not implemented");
+    });
+
+    it("should retrieve the correct entry in localStorage", () => {
+        //TODO
+        fail("Test not implemented");
+    });
+});
+
+describe("Testing DataHandler operations", () => {
+    it("should increase boxCount with the correct value", () => {
+        //TODO
+        fail("Test not implemented");
+    });
+
+    it("should increase the correct type of item", () => {
+        //TODO
+        fail("Test not implemented");
+    });
+});
